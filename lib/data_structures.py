48c10e4154657c013ae0f79cf08db10835c997c8
Guang <jouyang19@proton.me>
2024-06-04 17:59:58 -0400

    complete lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,74 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = []
+    for food in spicy_foods:
+        names.append(food['name'])
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciestArr = []
+    for food in spicy_foods:
+        if food['heat_level'] > 5:
+            spiciestArr.append(food)
+    return spiciestArr
+
+get_spiciest_foods(spicy_foods)
 
 def print_spicy_foods(spicy_foods):
-    pass
+    new_list = []
+    for food in spicy_foods:
+        name = food['name']
+        cuisine = food['cuisine']
+        heat_level = food['heat_level'] * "🌶"
+        (new_list.append(f'{name} ({cuisine}) | Heat Level: {heat_level}'))
+    for item in new_list:
+        print(item)
+    
+print_spicy_foods(spicy_foods)
+        
+        
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food['cuisine'] == cuisine:
+            return food
+        else:
+            None
+print(get_spicy_food_by_cuisine(spicy_foods, 'American'))
+print(get_spicy_food_by_cuisine(spicy_foods, 'Thai'))
+print(get_spicy_food_by_cuisine(spicy_foods, 'Sichuan'))
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    new_list = []
+    for food in spicy_foods:
+        if food['heat_level'] > 5:
+            name = food['name']
+            cuisine = food['cuisine']
+            heat_level = food['heat_level'] * '🌶'
+            new_list.append(f'{name} ({cuisine}) | Heat Level: {heat_level}')
+    else:
+        None
+    for item in new_list:
+        print(item)
+print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    sum = 0
+    for food in spicy_foods:
+        sum = sum + food['heat_level']
+    return sum / len(spicy_foods)
+get_average_heat_level(spicy_foods)
+        
+        
+spicy_food = {
+    'name': 'Griot',
+    'cuisine': 'Haitian',
+    'heat_level': 10
+}
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    new_spicy_foods = spicy_foods
+    new_spicy_foods.append(spicy_food)
+    return new_spicy_foods
+print(create_spicy_food(spicy_foods, spicy_food))
